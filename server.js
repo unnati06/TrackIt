8294826f050b0de2d42ca4fdaa5109fd54f25345
unnati06 <137003791+unnati06@users.noreply.github.com>
2024-08-26 20:08:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
 import helmet from 'helmet';
@@ -36,21 +35,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
-
-//cors
-const corsOptions = {
-  origin: (origin, callback) => {
-    
-    if (!origin) return callback(null, true);
-    callback(null, true); 
-  },
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
@@ -67,18 +51,9 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-const distPath = path.join(__dirname, 'client', 'dist');
-console.log('Serving static files from:', distPath);
-app.use(express.static(distPath));
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
-
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
@@ -86,7 +61,7 @@ app.use('*', (req, res) => {
 
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5100;
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
@@ -94,6 +69,6 @@ try {
     console.log(`server running on PORT ${port}...`);
   });
 } catch (error) {
-  console.log("error starting server");
+  console.log(error);
   process.exit(1);
 }
